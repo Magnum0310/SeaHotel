a0d9aa0854fd0b24132112c6706606e46f3d412a
Pancho <test@email.com>
2023-09-07 10:25:52 +0800

    Initial commit

---
+++ src/layouts/RootLayout.js
@@ -0,0 +1,46 @@
+import {NavLink,Outlet} from "react-router-dom"
+
+export default function RootLayout(){
+
+    function getYear(){
+        return new Date().getFullYear()
+    }
+
+    return(
+        <div className="root-layout">
+            <header>
+                <nav>
+                    <ul>
+                        <NavLink to="room" className="roomHightlight">Rooms</NavLink>
+                        <NavLink to="dinning" className="dinningHightlight">Dinning</NavLink>
+                        <NavLink to="event" className="eventHightlight">Events</NavLink>
+                    </ul>
+                </nav>
+            </header>
+        <nav className="sideBar">
+            <li className="sideBarLogo instagram"><span></span></li>
+            <li className="sideBarLogo facebook"><span></span></li>
+            <li className="sideBarLogo pinterest"><span></span></li>
+        </nav>
+
+            <main>
+                <Outlet/>
+            </main>
+
+            <footer>
+                <div className="footerArea">
+                    <div className="hotelLogo"><h1>Sea Hotel</h1></div>
+                    <div className="hotelContactDetails">
+                        <ul>
+                            <li>Main address Plus Street</li>
+                            <li>+99 9 9999 9999</li>
+                            <li>+99 9 9999 9999</li>
+                            <li>site@seahotel.com</li>
+                        </ul>
+                    </div>
+                    <div className="copyright">© Copyright {getYear()} </div>
+                </div>
+            </footer>
+        </div>
+    )
+} 
---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+import React from "react"
+import Building from "../Building_Facade.jpg"
+
+
+export default function Home(){
+
+    return(
+    <div className="homePage">
+        <div className="centerImage">
+            <h1>Sea Hotel</h1> 
+        </div>
+        <div className="aboutUs">
+            <h2>Welcome to Sea Hotel</h2>
+            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</h3>
+        </div>
+        <div className="room">
+        </div>
+        <div className="hotelRooms">
+            <div className="roomDesciption">
+                <h3>Rooms</h3>
+                <h2>Room description</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p> 
+            </div>
+            <div className="roomImage"></div>
+        </div>
+        <div className="dining">
+            <div className="dinningImage"></div>
+            <div className="dinningDesciption">
+                <h3>Dinning</h3>
+                <h2>Something about dinning</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p> 
+            </div>
+        </div>
+        <div className="events">
+                <div className="wedding">Wedding</div>
+                <div className="eventDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+                <div className="surf">Surfing</div>
+        </div>
+    </div>
+    )
+
+}
